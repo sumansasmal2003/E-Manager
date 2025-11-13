d4ba197f43163827d3948a6adb1ee6cedbd35250
SUMAN SASMAL <sasmalsuman04@gmail.com>
2025-11-13 08:11:23 +0530

    api

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://e-manager-backend-6vwv.onrender.com/api', // Your backend URL
-  baseURL: 'http://localhost:5000/api', // Your backend URL
+  baseURL: 'https://e-manager-backend-6vwv.onrender.com/api', // Your backend URL
+  // baseURL: 'http://localhost:5000/api', // Your backend URL
 });
 
 // Interceptor to add the token to every request
